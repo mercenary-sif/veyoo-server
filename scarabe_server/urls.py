7053f5ba13d22ab929dcd39711ad67224f42f1cc
Aguaguena Sif Eddine <koroashinosif@gmail.com>
2025-08-19 02:50:23 +0200

    Update urls.py

--- scarabe_server/urls.py
+++ scarabe_server/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from scarabe import views
+from ..scarabe import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/create-account/', views.CreateAccountAPIView.as_view(), name='create-account'),
